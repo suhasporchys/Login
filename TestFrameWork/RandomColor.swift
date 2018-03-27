a99df313fba77a6e25acf8be7a8dc97cfc967249
suhas <suhas.porchys@gmail.com>
2018-03-27 15:45:02 +0530

    initialize UIview

---
+++ TestFrameWork/RandomColor.swift
@@ -0,0 +1,36 @@
+//
+//  RandomColor.swift
+//  TestFrameWork
+//
+//  Created by CSOIM01181089M on 1/25/18.
+//  Copyright © 2018 CSOIM01181089M. All rights reserved.
+//
+
+import Foundation
+public func randomRGBColor() -> UIColor {
+    return .randomColor()
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+
+extension UIColor {
+    static func randomColor() -> UIColor {
+        
+        return UIColor(red: .random(), green: .random(), blue: .random(), alpha: 1.0)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
